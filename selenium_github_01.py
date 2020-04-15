69abd01dc3e8fba10cf061256e432b24ee86bf9e
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-15 16:16:27 +0800

    Initial commit 3

--- selenium_github_01.py
+++ selenium_github_01.py
@@ -1,4 +1,18 @@
-
-
+# This is a github selenium with pycharm - Test all elements for second time
+import time
 import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://www.newtours.demoaut.com/")
+
+print(driver.title)
+
+time.sleep(5)
+
+driver.close()
+
+
 
