4075bdd89fa0fae65adb2efd4e66d200bf65006b
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-15 16:23:36 +0800

    Initial commit 4

--- selenium_github_01.py
+++ selenium_github_01.py
@@ -8,7 +8,18 @@ driver = webdriver.Firefox()
 
 driver.get("http://www.newtours.demoaut.com/")
 
+# This is a title of the page
+time.sleep(1)
 print(driver.title)
+
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+
+# This is return HTML code form the page
+# print(driver.page_source)
 
 time.sleep(5)
 
