6fe81e6423f5fecf6124e327f3240237819052c7
motovmp1 <vinicius.mpinho@gmail.com>
2020-04-15 16:32:48 +0800

    Initial commit

---
+++ selenium_github_02.py
@@ -0,0 +1,30 @@
+# This is a github selenium with pycharm - Test all elements for second time
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://demo.automationtesting.in/Windows.html")
+
+# This is a title of the page
+time.sleep(1)
+print(driver.title)
+
+
+# This is get URL of the page
+time.sleep(1)
+print(driver.current_url)
+
+# click button to next page
+time.sleep(2)
+driver.find_element_by_xpath("//div[@id='Tabbed']//button[@class='btn btn-info'][contains(text(),'click')]").click()
+
+# This is return HTML code form the page
+# print(driver.page_source)
+
+time.sleep(5)
+
+driver.close()
+
